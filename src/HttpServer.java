1d79a1d64e48bc81138a24107454f4fadc24d06c
Horus137 <a93219@alunos.uminho.pt>
2021-12-23 18:48:06 +0000

    http runnable

--- src/HttpServer.java
+++ src/HttpServer.java
@@ -4,42 +4,63 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 
-public class HttpServer {
+public class HttpServer implements Runnable{
 
-    //private int port = peer.port;
+    public boolean run;
+    public int port;
+    public ServerSocket serverSocket;
+    public String message;
 
-    public static void main(String[] args) throws  IOException{
 
-        int port = 8989;
-        //System.out.println("Server starting...");
-        //System.out.println("Using Port: " + conf.getPort());
+    public static void server (String ip){
 
-        ServerSocket serverSocket = new ServerSocket(port);
-        System.err.println("Server running on port: "+ port);
-
-        while(true){
-            Socket clientSocket = serverSocket.accept();
-            System.err.println("Client connected");
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            String s;
-            while((s = in.readLine())!=null){
-                System.out.println(s);
-                if(s.isEmpty()){
-                    break;
-                }
-            }
-            OutputStream clientOutput = clientSocket.getOutputStream();
-            clientOutput.write("HTTP/1.1 200 OK\r\n".getBytes());
-            clientOutput.write("\r\n".getBytes());
-            clientOutput.write("<b>FT-Rapid</b>".getBytes());
-            clientOutput.write("\r\n\r\n".getBytes());
-            clientOutput.flush();
-            System.err.println("Client connection closed!");
-            in.close();
-            clientOutput.close();
+        int port = 80;
+        boolean run = true;
+        try{
+            ServerSocket serverSocket = new ServerSocket(port);
+            //System.out.println("Server starting...");
+            //System.out.println("Using Port: " + conf.getPort());
+            System.err.println("Server running on port: "+ port);
+        }
+        catch (IOException e){
+            e.printStackTrace();
         }
 
+        String message = "Inicializando";
+    }
 
+    public void newMesage(String message){}
 
+    @Override
+    public void run() {
+        try{
+            while (run) {
+
+                Socket clientSocket = serverSocket.accept();
+                System.err.println("Client connected");
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                String s;
+                while((s = in.readLine())!=null){
+                    System.out.println(s);
+                    if(s.isEmpty()){
+                        break;
+                    }
+                }
+
+                OutputStream clientOutput = clientSocket.getOutputStream();
+                clientOutput.write("HTTP/1.1 200 OK\r\n".getBytes());
+                clientOutput.write("\r\n".getBytes());
+                String m = "<b>" + message + "</b>";
+                clientOutput.write(m.getBytes());
+                clientOutput.write("\r\n\r\n".getBytes());
+                clientOutput.flush();
+                System.err.println("Client connection closed!");
+                in.close();
+                clientOutput.close();
+            }
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
